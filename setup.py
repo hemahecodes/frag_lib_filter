a121c4e6398e90a3ffee2ed81ea35ab8167d92d4
hemahecodes <helena.martin@alum.esci.upf.edu>
2020-10-30 17:19:35 +0100

    setup.py for packaging

---
+++ setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="frag-lib-filter",
+    version="0.0.0",
+    author="Helena Martin",
+    author_email="helena.martin@nostrumbiodiscovery.com",
+    description="Fragment library package",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/hemahecodes/fragment_library",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
